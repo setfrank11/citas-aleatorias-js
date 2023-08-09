62f7df1af19562ee9d8f455383c1cf1211b1d357
setfrank11 <105731465+setfrank11@users.noreply.github.com>
2023-08-08 22:32:00 -0500

    agregando archivo HTML, CSS y JS, responsivo, cambia citas con click en button

---
+++ app.js
@@ -0,0 +1,15 @@
+let bontonElem = document.getElementById('boton-cambiar-cita');
+let citaElem = document.getElementById('cita');
+let autorElem = document.getElementById('autor');
+
+function generarEnteroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+function cambiarCita(){
+    let indiceAleatorio = generarEnteroAleatorio(0, citas.length);    
+    citaElem.innerText = `"${citas[indiceAleatorio].texto}"`;
+    autorElem.innerText = citas[indiceAleatorio].autor;
+}
+
+cambiarCita();
+bontonElem.addEventListener('click', cambiarCita);
